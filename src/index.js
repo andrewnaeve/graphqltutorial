789c63ea973ddc17de041dd4a62a380062b0dcb5
Andrew Naeve <andnaeve@amazon.com>
2019-06-25 13:43:18 -0700

    rm context

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import schema from './graphql/schema';
 import tables from './config/tables';
 
 const server = new ApolloServer({
-  schema,
-  context: ({ context }) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-    return {
-      ...tables
-    };
-  }
+  schema
 });
 
 export const handler = server.createHandler({
