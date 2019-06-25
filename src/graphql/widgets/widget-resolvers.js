ab3be276394b8c7d4fc1b9969f9aaef5e6754538
Andrew Naeve <andnaeve@amazon.com>
2019-06-25 13:26:20 -0700

    blank slate

--- src/graphql/widgets/widget-resolvers.js
+++ src/graphql/widgets/widget-resolvers.js
@@ -2,16 +2,7 @@ import WidgetService from './widget-service';
 
 const widgetResolvers = ({ dynamodb }) => {
   const service = WidgetService({ dynamodb });
-  return {
-    Query: {
-      getWidget: (_, { widget_id: widgetId }, context) =>
-        service.getWidget({ widgetId, ...context })
-    },
-    Mutation: {
-      saveWidget: (_, { widget }, context) =>
-        service.saveWidget({ widget, ...context })
-    }
-  };
+  return {};
 };
 
 export default widgetResolvers;
--- src/graphql/widgets/widget-service.js
+++ src/graphql/widgets/widget-service.js
@@ -1,36 +1,9 @@
 import { v4 as keygen } from 'uuid';
 
 function WidgetService({ dynamodb }) {
-  async function getWidget({ widgetId, widgetTableName }) {
-    const params = {
-      TableName: widgetTableName,
-      Key: {
-        widget_id: widgetId
-      }
-    };
+  async function getWidget({ widgetId, widgetTableName }) {}
 
-    const { Item } = await dynamodb.get(params).promise();
-
-    return Item;
-  }
-
-  async function saveWidget({ widget, widgetTableName }) {
-    const params = {
-      TableName: widgetTableName,
-      Item: {
-        widget_id: keygen(),
-        ...widget
-      }
-    };
-    try {
-      await dynamodb.put(params).promise();
-    } catch (error) {
-      throw error;
-    }
-    return {
-      success: true
-    };
-  }
+  async function saveWidget({ widget, widgetTableName }) {}
 
   return Object.freeze({
     getWidget,
